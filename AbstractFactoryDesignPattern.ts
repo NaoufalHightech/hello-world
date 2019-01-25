19faf8ab4228cac52ec8e49fb03545f80dc56f96
Raghava <raghava219@gmail.com>
2019-01-25 06:28:04 +0530

    Modified Typescript example at home - 25-Jan.

--- AbstractFactoryDesignPattern.ts
+++ AbstractFactoryDesignPattern.ts
@@ -3,19 +3,25 @@ interface Bank{
     getIntrestRate(): number;
 }
 
+abstract class AbstractFactory{
+    abstract getBank(bank: string):Bank;
+    abstract getLoan(loan: string):Loan;
+}
+
 class KOTAK implements Bank{
    
     getBankDetails(): string{
-        return "SBI Bank";
+        return "KOTAK Bank";
     }
 
     getIntrestRate():number{
         return 8.4;
     }
 }
+
 class IDFC implements Bank{
     getBankDetails(): string{
-        return "HDFC Bank";
+        return "IDFC Bank";
     }
     getIntrestRate():number{
         return 7.4;
@@ -24,32 +30,28 @@ class IDFC implements Bank{
 
 abstract class Loan{
     protected rate: number;
-    abstract getIntrestRate(rate : number):void;
+    abstract setIntrestRate(rate : number):void;
     public calculateLoadPayment(loanamount:number, years:number):void{
         let EMI:number;
         let n:number;
 
         n = years * 12;
         this.rate = this.rate/12;
-        EMI=((this.rate*Math.pow((1+this.rate),n))/((Math.pow((1+this.rate),n))-1))*loanamount;  
-        console.log("your monthly EMI is "+ EMI +" for the amount"+loanamount+" you have borrowed");
+        EMI = ((this.rate * Math.pow((1+this.rate),n)) / ((Math.pow((1+this.rate),n))-1)) * loanamount;  
+        console.log("your monthly EMI is "+ EMI +" for the amount "+loanamount+" you have borrowed");
     }
 }
 
 class  HomeLoan extends Loan{
-    public getIntrestRate(r:number){
+    public setIntrestRate(r:number){
         this.rate = r;
     }
 }
 class  BusinessLoan extends Loan{
-    public getIntrestRate(r:number){
+    public setIntrestRate(r:number){
         this.rate = r;
     }
-    
-abstract class AbstractFactory{
-    abstract getBank(bank: string):Bank;
-    abstract getLoan(loan: string):Loan;
-}
+}    
 
 class BankFactory extends AbstractFactory{
     getBank(bank:string): Bank{  
@@ -100,26 +102,26 @@ class FactoryCreator {
 }
 
 
-console.log("Enter the name of Bank from where you want to take loan amount: ");  
-let bankName: string ="SBI";  
+//console.log("Enter the name of Bank from where you want to take loan amount: ");  
+let bankName: string ="IDFC";  
   
-console.log("\n");  
-console.log("Enter the type of loan e.g. home loan or business loan or education loan : ");  
+//console.log("\n");  
+//console.log("Enter the type of loan e.g. home loan or business loan or education loan : ");  
   
 let loanName : string = "Home";  
 let bankFactory : AbstractFactory = FactoryCreator.getFactory("Bank");  
 let b: Bank = bankFactory.getBank(bankName);  
   
-console.log("\n");  
-console.log("Enter the interest rate for "+b.getBankDetails()+ ": ");  
+//console.log("\n");  
+//console.log("Enter the interest rate for "+b.getBankDetails()+ ": ");  
   
 let rate : number = 23;  
-console.log("\n");  
-console.log("Enter the loan amount you want to take: ");  
+//console.log("\n");  
+//console.log("Enter the loan amount you want to take: ");  
   
 let loanAmount : number = 40000;  
-console.log("\n");  
-console.log("Enter the number of years to pay your entire loan amount: ");  
+//console.log("\n");  
+//console.log("Enter the number of years to pay your entire loan amount: ");  
 let years : number = 3;  
   
 console.log("\n");  
@@ -127,18 +129,28 @@ console.log("you are taking the loan from "+ b.getBankDetails());
   
 let loanFactory : AbstractFactory = FactoryCreator.getFactory("Loan");  
 
-let l : Loan = loanFactory.getLoan(loanName);  
-l.calculateLoadPayment(loanAmount,years)
-l.getIntrestRate(rate);
-           
+let l : Loan = loanFactory.getLoan(loanName);
+l.setIntrestRate(rate);  
+l.calculateLoadPayment(loanAmount,years);
 
 
+/**** Below is the example for closures */
+
+var addTo = function(passed){
+
+    var add = function(inner){
+        return passed + inner;
+    }
+
+    return add;
+}
+
+var addThree = addTo(3);
+var addFour = addTo(4);
 
 
-
-
-
-
+console.log(addThree(1));
+console.log(addFour(1));
 
 
 
